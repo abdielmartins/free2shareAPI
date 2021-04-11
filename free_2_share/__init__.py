240c1ceb2af69700e84ef57c59f3350d775a8485
abdielmartins <abdielmartins21@gmail.com>
2021-04-11 17:09:36 -0300

    feat: added user_get route

--- free_2_share/__init__.py
+++ free_2_share/__init__.py
@@ -6,6 +6,8 @@ from config import config_selector
 from free_2_share.configurations import database
 from free_2_share.configurations import migration
 
+from free_2_share import views
+
 
 def create_app():
     app = Flask(__name__)
@@ -14,5 +16,6 @@ def create_app():
 
     database.init_app(app)
     migration.init_app(app)
+    views.init_app(app)
 
     return app
--- free_2_share/views/user_view.py
+++ free_2_share/views/user_view.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request, current_app
+from flask.globals import session
 from free_2_share.models.user_model import UserModel
 from http import HTTPStatus
 
@@ -18,7 +19,7 @@ def create_user():
         password = body["password"]
         phone = body["phone"]
         profile_pic_url = body["link_profile_picture"]
-        bio = body["body"]
+        bio = body["bio"]
 
         new_user = UserModel(
             name=name,
@@ -38,13 +39,59 @@ def create_user():
             "user": {
                 "id": new_user.id,
                 "name": new_user.name,
-                "nickname": nickname,
-                "email": email,
-                "phone": phone,
-                "link_profile_picture": profile_pic_url,
-                "bio": bio,
+                "nickname": new_user.nickname,
+                "email": new_user.email,
+                "phone": new_user.phone,
+                "link_profile_picture": new_user.link_profile_picture,
+                "bio": new_user.bio,
+                "favorites": new_user.favorites_list,
             }
-        }
+        }, HTTPStatus.CREATED
 
     except KeyError:
         return {"message": "Could not create new user! Verify your request body"}, HTTPStatus.BAD_REQUEST
+
+
+@bp_user.route("/get", methods=["GET"])
+def list_all_users():
+    name_filter = request.args.get("name")
+
+    if name_filter:
+        list_of_users: UserModel = (
+            UserModel.query.filter(UserModel.name.like(f"%{name_filter}%")).order_by(UserModel.name).all()
+        )
+    else:
+        list_of_users: UserModel = UserModel.query.all()
+
+    return {
+        "users": [
+            {
+                "name": user.name,
+                "nickname": user.nickname,
+                "email": user.email,
+                "phone": user.phone,
+                "bio": user.bio,
+                "favorites": user.favorites_list,
+            }
+            for user in list_of_users
+        ]
+    }, HTTPStatus.OK
+
+
+@bp_user.route("/update/<int:user_id>", methods=["PATCH", "PUT"])
+def update_user(user_id):
+    KEYS_LIST = ("name", "nickname", "email", "password", "phone", "profile_pic_url", "bio")
+
+    session = current_app.db.session
+
+    body = request.get_json()
+
+    values_to_update = {}
+
+    for key in KEYS_LIST:
+        if body.get(key):
+            values_to_update[key] = body.get(key)
+
+    print(values_to_update)
+
+    return HTTPStatus.OK
