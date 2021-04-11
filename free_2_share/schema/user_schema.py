72cfd842381e7908524562577d2369c1f1c5401e
abdielmartins <abdielmartins21@gmail.com>
2021-04-11 20:56:14 -0300

    feat: added user_schema, wip on user_model

---
+++ free_2_share/schema/user_schema.py
@@ -0,0 +1,20 @@
+from . import ma
+from free_2_share.models.user_model import UserModel
+
+
+class UserSchema(ma.Schema):
+    class Meta:
+        model = UserModel
+
+    id = ma.Integer()
+    name = ma.String()
+    nickname = ma.String()
+    email = ma.String()
+    phone = ma.String()
+    link_profile_picture = ma.String()
+    bio = ma.String()
+    favorites_list = ma.List()
+
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
