4a7e09f21b4e070b8f134c152e13bbe36ec3102a
abdielmartins <abdielmartins21@gmail.com>
2021-04-11 21:42:00 -0300

    feat: changed from blueprint to flask-restfull, added new schema and view configs

--- free_2_share/configurations/views.py
+++ free_2_share/configurations/views.py
@@ -4,11 +4,24 @@ from flask_restful import Api
 
 def init_app(app: Flask):
     api = Api(app)
+
+    from free_2_share.views.user_view import User, AllUsers
     from free_2_share.views.card_view import Card, AllCards
+
+    api.add_resource(AllUsers, "/api/users")
+    api.add_resource(User, "/api/user", endpoint="/user", methods=["POST"])
+    api.add_resource(
+        User,
+        "/api/user/<int:user_id>",
+        endpoint="/user/<int:user_id>",
+        methods=["GET", "PATCH", "DELETE"],
+    )
+
     api.add_resource(AllCards, "/api/card")
     api.add_resource(Card, "/api/card", endpoint="/card", methods=["POST"])
-    api.add_resource(Card,
-                     "/api/card/<int:card_id>",
-                     endpoint="/card/<int:user_id>",
-                     methods=["GET", "PATCH", "DELETE"],
-                     )
+    api.add_resource(
+        Card,
+        "/api/card/<int:card_id>",
+        endpoint="/card/<int:user_id>",
+        methods=["GET", "PATCH", "DELETE"],
+    )
